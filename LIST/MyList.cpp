15f4122dd1f6fa4a71d971f5e64713d13680fe47
Zhenya Khilko <khilkoev@mail.ru>
2021-06-19 15:58:21 +0300

    'message'

---
+++ LIST/MyList.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "List.h"
+#include <algorithm>
+#include <cassert>
+
+const int ar = 109;
+
+constexpr int getArrayValue(int* arr) 
+{
+    static_assert(ar >= 0 && ar < 10,"Wrong index");
+
+    return arr[ar];
+}
+
+int main()
+{
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int a = getArrayValue(arr);
+   
+    std::cout << a;
+}
+
